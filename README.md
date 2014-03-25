00624c69d0e5be7557b7dcce131d77a8b8defd65
Chris Barrett <chris.barrett@opower.com>
2014-03-25 00:12:35 -0700

    api endpoint dumps html headers

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 TODO
 ----
 * fix http endpoint that echoes remote ip - always returns 127.0.0.1
+* hide sinatra error page
 * add endpoint to html page
 * benchmark requests/sec
 * measure memory usage during benchmark
--- api/api.rb
+++ api/api.rb
@@ -1,8 +1,15 @@
 #!/usr/bin/ruby
 require 'rubygems'
+require 'rack'
 require 'sinatra'
 
+Process.daemon()
+
 get '/' do
-  request.ip
+  headers = request.env
+    .collect {|pair| [ Rack::Utils.escape_html(pair[0]), Rack::Utils.escape_html(pair[1]) ]}
+    .collect {|pair| pair.join(": ") << "<br>"}
+    .sort
+  [200, {'Content-Type' => 'text/html'}, headers]
 end
 
